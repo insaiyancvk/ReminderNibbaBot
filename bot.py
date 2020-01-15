284071587131cd909533ae2c62c10725c48e9546
insaiyancvk <cvashik1@gmail.com>
2020-01-15 19:01:03 +0530

    first commit

---
+++ bot.py
@@ -0,0 +1,10 @@
+from bottle import run,post,request as bottle_request
+
+def main():
+	data = bottle_request.json
+	print(data)
+    return
+
+if __name__=='__main__':
+	   run(host='localhost', port=8080, debug = True)
+
